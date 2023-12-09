07acf5f423edf8a09017fc7e3180901b515720d2
Hellen Onyango <honyango95@gmail.com>
2023-12-09 12:07:07 +0000

    Adds code updates

--- milestone_3.py
+++ milestone_3.py
@@ -6,25 +6,27 @@ import random
 word_list = ['mango', 'passion', 'papaya', 'guava', 'oranges']
 
 #Selects random fruits from list
-word = random.choice(word_list)
+generated_random_word = random.choice(word_list)
 
-def check_guess(guess):  
+def check_guess(lower_user_letter_guess):  
   #Checks if users input is in the random word 
-    if guess in word:
-        print (f'Good guess! {guess} is in the word.')
+    if lower_user_letter_guess in generated_random_word:
+        print (f'Good guess! {lower_user_letter_guess} is in the word.')
     else: 
-        print(f'Sorry, {guess} is not in the word. Try again.') 
+        print(f'Sorry, {lower_user_letter_guess} is not in the word. Try again.') 
 
 def ask_for_input():
 
     #Checks if users input format is correct
       while True:
-        guess = input('Enter a single letter here:' )
-        if len(guess) == 1 and guess.isalpha():
+        user_letter_guess = input('Enter a single letter here:' )
+        #Converts the input to lower case
+        lower_user_letter_guess = user_letter_guess.lower()
+        if len(lower_user_letter_guess) == 1 and lower_user_letter_guess.isalpha():
             break
         else: 
           print("Invalid letter. Please, enter a single alphabetical character.") 
-      check_guess(guess)
+      check_guess(lower_user_letter_guess)
 
 ask_for_input()
 
