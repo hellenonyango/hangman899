4253b6c1f6a1bcfd7f02f7411c99654445df7c75
Hellen Onyango <honyango95@gmail.com>
2023-12-09 12:06:45 +0000

    Adds code updates

--- milestone_2.py
+++ milestone_2.py
@@ -1,15 +1,14 @@
 
 #%%
 import random
-
 #list of favourite fruits 
 word_list = ['mango', 'passion', 'papaya', 'guava', 'oranges']
 
-#Select random fruits from list
-word = random.choice(word_list)
+#Selects random fruits from list
+generated_random_word = random.choice(word_list)
 
 #prints random fruits every time function is run
-print(word)
+print(generated_random_word)
 
 #%%
 #Iterate through whole list and returns random list each time
@@ -19,11 +18,11 @@ for name_of_fruit in word_list:
 # %%
 
 #Asks users to guess by letter input
-guess = input('Enter a single letter here:' )
+user_letter_guess = input('Enter a single letter here:' )
 
 #Checks that the correct data type is entered
 
-if len(guess) == 1 and guess.isalpha():
+if len(user_letter_guess) == 1 and user_letter_guess.isalpha():
  print("Good guess!")
 else: 
  print("Oops! That is not a valid input.")
